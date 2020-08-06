29bbf2fe999aa2fd4d243b3fc0e30361a8120fd7
Chris Carty <chriscarty@google.com>
2020-08-06 15:06:34 -0400

    added test script and cloudbuild file

---
+++ tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download conftest if not installed
+if ! conftest --version &> /dev/null
+then
+	echo "Installing conftest"
+	wget https://github.com/open-policy-agent/conftest/releases/download/v0.20.0/conftest_0.20.0_Linux_x86_64.tar.gz
+	tar xzf conftest_0.20.0_Linux_x86_64.tar.gz
+	sudo mv conftest /usr/local/bin
+fi
+
+# Testing rego
+echo "Testing Policies
+for guardrail in guardrails/*; do
+	if [ -d "$guardrail" ]; then
+		echo $guardrail
+		conftest verify -p $guardrail
+	fi
+done
